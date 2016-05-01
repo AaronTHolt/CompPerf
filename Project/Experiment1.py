8a554699c3622979440ddcd1e75f3542515ca171
AaronTHolt <holtat@colorado.edu>
2016-05-01 15:49:21 -0600

    Add csv results for experiments

---
+++ Project/Experiment1.py
@@ -0,0 +1,177 @@
+import os
+import subprocess
+from pyDOE import *
+import csv
+
+
+#Experiment 1:
+# 4*2*2
+# O[0-4], -DSTREAM_TYPE=float (single precision variables on or off), -m32 vs -m64
+
+
+#compile and run experiment?
+run = False
+
+### Create experiment
+table = ff2n(2)
+# print(table)
+
+all_data = {}
+full_table = []
+
+commands = []
+filenames = []
+
+cmd_base = "cc stream.c -o "
+
+for opt in [0,1,2,3]:
+	
+
+    for exp in table:
+        cmd_opts = ""
+        O_level = " -O"+str(opt)
+        cmd_opts += O_level
+
+        file_output = "Exp1/"
+        file_output += "O" + str(opt)
+
+        if exp[0] > 0:
+            cmd_opts += " -m32"
+            file_output += "_32"
+        else:
+            cmd_opts += " -m64"
+            file_output += "_64"
+
+        if exp[1] < 0:
+            cmd_opts += " -DSTREAM_TYPE=float"
+            file_output += "_S"
+        else:
+            file_output += "_D"
+
+        for jj in range(3):
+            full_table.append([-1,opt,exp[0],exp[1],-1,-1])
+        filenames.append(file_output)
+        commands.append(cmd_base+file_output+cmd_opts)
+
+all_data['copy'] = list(full_table)
+all_data['scale'] = list(full_table)
+all_data['add'] = list(full_table)
+all_data['triad'] = list(full_table)
+
+if run == True:
+    ### Compile Experiment
+    for cmd in commands:
+        print(cmd)
+        subprocess.call(cmd, shell=True)
+        process = subprocess.Popen(cmd, shell=True, \
+            stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL).communicate()[0]
+
+
+    ### Run Experiment
+    for trial in filenames:
+        for replication in range(3):
+            #overwrite file on first repl
+            if replication == 0:
+                print("./"+trial + " > " + trial+".out")
+                process = subprocess.Popen("./"+trial + " >> " + trial+".out", shell=True)
+                process.wait()
+            else:
+                print("./"+trial + " >> " + trial+".out")
+                process = subprocess.Popen("./"+trial + " >> " + trial+".out", shell=True)
+                process.wait()
+
+
+def str_to_float(stream_list):
+    stream_list.pop(0)
+    for ii, col in enumerate(stream_list):
+            stream_list[ii] = float(stream_list[ii])
+
+    return stream_list
+
+def get_index(filename, replications, cur_repl):
+    index = 0
+
+    #optimization level
+    index += int(float(filename[6]))*12
+
+    #32 or 62 bit
+    if "_64" in filename:
+        index += 2*replications
+
+    #single precision?
+    if "_S" in filename:
+        index += 1*replications
+
+    #replication number
+    index += cur_repl
+
+    return index
+
+def stream_data_to_csv(filename, csvfile, replications):
+    '''	filename is stream data file
+        csvfile is the csv to write to
+        replications is the number of times each setup was run'''
+    content = []
+    with open(filename) as f:
+        content = f.readlines()
+    f.close()
+
+    repl = 0
+
+    for line in content:
+        index = get_index(filename, replications, repl)
+
+
+        if "Copy:" in line:
+            data = str_to_float(line.split())
+            all_data['copy'][index][0] = repl
+            all_data['copy'][index][4] = data[0]
+            all_data['copy'][index][5] = data[1]
+
+        elif "Scale:" in line:
+            data = str_to_float(line.split())
+            all_data['scale'][index][0] = repl
+            all_data['scale'][index][4] = data[0]
+            all_data['scale'][index][5] = data[1]
+
+        elif "Add:" in line:
+            data = str_to_float(line.split())
+            all_data['add'][index][0] = repl
+            all_data['add'][index][4] = data[0]
+            all_data['add'][index][5] = data[1]
+
+        elif "Triad:" in line:
+            data = str_to_float(line.split())
+            all_data['triad'][index][0] = repl
+            all_data['triad'][index][4] = data[0]
+            all_data['triad'][index][5] = data[1]
+
+            ##next replication after triad
+            repl += 1
+
+    
+    for ii in files:
+        with open(csv_prefix + ii + '.csv', 'wt') as outcsv:
+            writer = csv.writer(outcsv)
+            writer.writerow(['Repl', 'O Level', '32b or 62b', 'Single Pres', 'Best Rate', 'Avg Time'])
+            writer.writerows(all_data[ii])
+        outcsv.close()
+
+
+
+### Process Data
+csv_prefix = "Exp1/"
+
+files = ['copy', 'scale', 'add', 'triad']
+
+for ii in files:
+    #clear contents of old files
+    open(csv_prefix + ii + '.csv', 'w').close()
+
+
+for exp in sorted(filenames):
+    # print(exp)
+
+    outfile = exp+".out"
+    # print(outfile)
+    stream_data_to_csv(outfile, csv_prefix, 3)
---
+++ Project/Experiment2.py
@@ -0,0 +1,265 @@
+import os
+import subprocess
+from pyDOE import *
+import csv
+
+
+#Experiment 1:
+# 4*2*2
+# O[0-4], -DSTREAM_TYPE=float (single precision variables on or off), -m32 vs -m64
+
+
+#compile and run experiment?
+comp = False
+run = False
+
+
+# print(table)
+
+all_data = {}
+full_table = []
+
+commands = []
+filenames = []
+
+cmd_base = ['cc', 'stream.c', '-o']
+
+
+#O3 opts
+opt_list = ['inline-functions', 'unswitch-loops', 'predictive-commoning', 'gcse-after-reload', 
+ 'tree-loop-distribute-patterns', 'tree-slp-vectorize', 'vect-cost-model', 'tree-partial-pre', 'ipa-cp-clone']
+
+#these ones not found 'tree-loop-vectorize', 'split-paths'
+
+### Create experiment
+# print(len(opt_list))
+table = ff2n(len(opt_list))
+# table = pbdesign(44)
+print(table)
+print(len(table))
+print(len(table[0]))
+
+for exp in table:
+    full_command = []
+    cmd_opts = ['-O2']
+    t1 = [-1]
+
+    file_output = "Exp2/"
+    for ii, param in enumerate(exp):
+        if param > 0:
+            cmd_opts.append("-f"+opt_list[ii])
+            file_output += "1"
+            t1.append(1)
+        else:
+            # cmd_opts.append("-fno-"+opt_list[ii])
+            file_output += "0"
+            t1.append(0)
+    
+    #construct results table
+    t1.append(-1)
+    t1.append(-1)
+    for jj in range(3):
+        full_table.append(t1)
+
+    # file_output += ".out"
+
+    #Put compile command together
+    for item in cmd_base:
+        full_command.append(item)
+    full_command.append(file_output)
+    for item in cmd_opts:
+        full_command.append(item)
+
+    #store compile commands
+    commands.append(full_command)
+
+    #store output filenames
+    filenames.append(file_output)
+
+    
+
+
+all_data['copy'] = list(full_table)
+all_data['scale'] = list(full_table)
+all_data['add'] = list(full_table)
+all_data['triad'] = list(full_table)
+
+print(len(full_table))
+
+if comp == True:
+    ### Compile Experiment
+    for cmd in commands:
+        print(cmd)
+        subprocess.call(cmd)
+        process = subprocess.Popen(cmd, \
+            stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL).communicate()[0]
+
+if run == True:
+    ### Run Experiment
+    for trial in filenames:
+        for replication in range(3):
+            #overwrite file on first repl
+            if replication == 0:
+                print("./"+trial + " > " + trial+".out")
+                process = subprocess.Popen("./"+trial + " >> " + trial+".out", shell=True)
+                process.wait()
+            else:
+                print("./"+trial + " >> " + trial+".out")
+                process = subprocess.Popen("./"+trial + " >> " + trial+".out", shell=True)
+                process.wait()
+
+
+def str_to_float(stream_list):
+    stream_list.pop(0)
+    for ii, col in enumerate(stream_list):
+            stream_list[ii] = float(stream_list[ii])
+
+    return stream_list
+
+def get_index(filename, replications, cur_repl):
+    index = 0
+    exponent = 0
+
+    for ii, factor in enumerate(filename):
+        index += int(float(factor))*replications*(2**exponent)
+        exponent += 1
+
+    #replication number
+    index += cur_repl
+    # print(filename, factor, ii, index, 'cur_repl = ', cur_repl)
+
+    return index
+
+def stream_data_to_csv(all_data, filename, csv_prefix, replications):
+    ''' filename is stream data file
+        csv_prefix is the directory the csvs are in
+        replications is the number of times each setup was run'''
+    content = []
+    with open(csv_prefix+filename+".out") as f:
+        content = f.readlines()
+    f.close()
+
+    repl = 0
+    results = {}
+    results['copy'] = []
+    results['scale'] = []
+    results['add'] = []
+    results['triad'] = []
+
+    for line in content:
+        if repl >= replications:
+            break
+
+        index = get_index(filename, replications, repl)
+
+        if "Copy:" in line:
+            tt = 'copy'
+            # print("Repl = ", repl)
+            data = str_to_float(line.split())
+            all_data[tt][index][0] = repl
+            all_data[tt][index][10] = data[0]
+            all_data[tt][index][11] = data[1]
+
+            # print(all_data['copy'][index], repl, index)
+            with open(csv_prefix + tt + '.csv', 'ta') as outcsv:
+                writer = csv.writer(outcsv)
+                writer.writerow(all_data[tt][index])
+
+        elif "Scale:" in line:
+            tt = 'scale'
+            data = str_to_float(line.split())
+            all_data[tt][index][0] = repl
+            all_data[tt][index][10] = data[0]
+            all_data[tt][index][11] = data[1]
+
+            # print(all_data['copy'][index], repl, index)
+            with open(csv_prefix + tt + '.csv', 'ta') as outcsv:
+                writer = csv.writer(outcsv)
+                writer.writerow(all_data[tt][index])
+
+        elif "Add:" in line:
+            tt = 'add'
+            data = str_to_float(line.split())
+            all_data[tt][index][0] = repl
+            all_data[tt][index][10] = data[0]
+            all_data[tt][index][11] = data[1]
+
+            # print(all_data['copy'][index], repl, index)
+            with open(csv_prefix + tt + '.csv', 'ta') as outcsv:
+                writer = csv.writer(outcsv)
+                writer.writerow(all_data[tt][index])
+
+        elif "Triad:" in line:
+            tt = 'triad'
+            data = str_to_float(line.split())
+            all_data[tt][index][0] = repl
+            all_data[tt][index][10] = data[0]
+            all_data[tt][index][11] = data[1]
+
+             # print(all_data['copy'][index], repl, index)
+            with open(csv_prefix + tt + '.csv', 'ta') as outcsv:
+                writer = csv.writer(outcsv)
+                writer.writerow(all_data[tt][index])
+
+            ##next replication after triad
+            repl += 1
+
+
+
+### Process Data
+csv_prefix = "Exp2/"
+
+files = ['copy', 'scale', 'add', 'triad']
+
+# print(len(filenames))
+
+for ii in files:
+    #clear contents of old files
+    open(csv_prefix + ii + '.csv', 'w').close()
+
+#Write csv header
+for ii in files:
+    with open(csv_prefix + ii + '.csv', 'wt') as outcsv:
+        writer = csv.writer(outcsv)
+        writer.writerow(['Repl', 'inline-functions', 'unswitch-loops', 'predictive-commoning', 'gcse-after-reload', 
+                        'tree-loop-distribute-patterns', 'tree-slp-vectorize', 'vect-cost-model', 'tree-partial-pre', 
+                        'ipa-cp-clone', 'Best Rate', 'Avg Time'])
+
+for exp in sorted(filenames):
+    # print(exp)
+
+    outfile = exp[5:]
+    # print(outfile)
+    stream_data_to_csv(all_data, outfile, csv_prefix, 3)
+
+
+# print(all_data['copy'][0:2])
+
+
+# print(all_data['copy'])
+
+
+
+
+
+
+# opt_list = [['auto-inc-dec', 'branch-count-reg', 'combine-stack-adjustments'],
+# ['compare-elim', 'cprop-registers' ,'dce'], ['defer-pop' ,'delayed-branch', 'dse'], 
+# ['forward-propagate', 'guess-branch-probability', 'if-conversion2'], 
+# ['if-conversion', 'inline-functions-called-once', 'ipa-pure-const'], 
+# ['ipa-profile', 'ipa-reference', 'merge-constants'], 
+# ['move-loop-invariants', 'reorder-blocks', 'shrink-wrap'], ['split-wide-types', 'ssa-backprop', 'ssa-phiopt'], 
+# ['tree-bit-ccp', 'tree-ccp', 'tree-ch'], ['tree-coalesce-vars', 'tree-copy-prop', 'tree-dce'],
+# ['tree-dominator-opts', 'tree-dse', 'tree-forwprop'], ['tree-fre', 'tree-phiprop', 'tree-sink'], 
+# ['tree-slsr', 'tree-sra', 'tree-pta'], ['tree-ter', 'unit-at-a-time']]
+
+## 01 ops
+# opt_list = ['auto-inc-dec', 'branch-count-reg', 'combine-stack-adjustments',
+# 'compare-elim', 'cprop-registers' ,'dce', 'defer-pop' ,'delayed-branch', 'dse', 
+# 'forward-propagate', 'guess-branch-probability', 'if-conversion2', 
+# 'if-conversion', 'inline-functions-called-once', 'ipa-pure-const', 
+# 'ipa-profile', 'ipa-reference', 'merge-constants', 
+# 'move-loop-invariants', 'reorder-blocks', 'shrink-wrap', 'split-wide-types', 'ssa-backprop', 'ssa-phiopt', 
+# 'tree-bit-ccp', 'tree-ccp', 'tree-ch', 'tree-coalesce-vars', 'tree-copy-prop', 'tree-dce',
+# 'tree-dominator-opts', 'tree-dse', 'tree-forwprop', 'tree-fre', 'tree-phiprop', 'tree-sink', 
+# 'tree-slsr', 'tree-sra', 'tree-pta', 'tree-ter', 'unit-at-a-time']
